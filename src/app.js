64472417057d9df491ecab90b2e3dd258504f742
eunice_sin_h <71563392+EuniceHG@users.noreply.github.com>
2023-06-16 17:58:23 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,66 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+/* Event listener for button click and initial console log */
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.getElementById("button").addEventListener("click", () => {
+    document.getElementById("the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello");
+};
+
+/* Excuse generator function */
+
+let generateExcuse = () => {
+  let who = [
+    "my little brother",
+    "my roommate",
+    "my boss",
+    "the aliens",
+    "my partner",
+    "the President",
+    "my imaginary friend"
+  ];
+  let what = ["hid", "ate", "stole", "erased", "sold", "forgot", "ate again"];
+  let avoid = [
+    "the cake",
+    "my mirror",
+    "our cat",
+    "the key",
+    "your food",
+    "my hair",
+    "your finger"
+  ];
+  let when = [
+    "last night",
+    "this morning",
+    "just now",
+    "five minutes ago",
+    "in another life",
+    "last year",
+    "before I was born"
+  ];
+  let sign = [".", "!", "?"];
+
+  /* Random index generation */
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let avoidIndex = Math.floor(Math.random() * avoid.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  let signIndex = Math.floor(Math.random() * sign.length);
+
+  /* Excuse generation */
+
+  let excuse =
+    who[whoIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    avoid[avoidIndex] +
+    " " +
+    when[whenIndex];
+
+  return excuse + sign[signIndex];
 };
